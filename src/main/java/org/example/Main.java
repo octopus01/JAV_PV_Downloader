45c87385f7525f0846ea5363b7fbdf04a3549ca1
home <390514378@qq.com>
2023-01-08 21:17:41 +0800

    修正

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,23 +13,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         String line;
-//        System.out.println("是否开启代理 Y/N");
-//        line= scanner.nextLine();
-//        if(line.equals("Y")||line.equals("y")) {
-//            System.out.println("选择代理类型 1.http 2.sock");
-//            line=scanner.nextLine();
-//            if (line.equals("1")) {
-//                System.out.println("请输入代理端口：");
-//                line=scanner.nextLine();
-//                ProxySettings.http(line);
-//            }
-//            else if (line.equals("2")){
-//                System.out.println("请输入代理端口：");
-//                line=scanner.nextLine();
-//                ProxySettings.socks(line);
-//            }
-//        }
-        ProxySettings.socks("10808");
+        System.out.println("是否开启代理 Y/N");
+        line= scanner.nextLine();
+        if(line.equals("Y")||line.equals("y")) {
+            System.out.println("选择代理类型 1.http 2.sock");
+            line=scanner.nextLine();
+            if (line.equals("1")) {
+                System.out.println("请输入代理端口：");
+                line=scanner.nextLine();
+                ProxySettings.http(line);
+            }
+            else if (line.equals("2")){
+                System.out.println("请输入代理端口：");
+                line=scanner.nextLine();
+                ProxySettings.socks(line);
+            }
+        }
+//        ProxySettings.socks("10808");
         System.out.println("请输入番号：如 ssis-557");
         JvmInfo jvmInfo = JvmInfo.getJvmInfo();
 
